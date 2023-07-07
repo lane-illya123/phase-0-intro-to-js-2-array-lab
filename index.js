9cc0691d3bc99fd58d4d0080ed3f424f487fedae
illyalane <lane_illya@yahoo.com>
2023-07-06 21:47:24 -0400

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+const copycats = [...cats , name];
+    return copycats;
+
+}
+
+function prependCat(name){
+    const copycats = [name , ...cats];
+    return copycats;
+}
+
+function removeLastCat(){
+    
+    return cats.slice(0,2);
+
+}
+
+function removeFirstCat(){
+
+    return cats.slice(1);
+}
